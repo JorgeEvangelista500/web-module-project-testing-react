ae0eb9ae5e45bdcd6c71e750c897d21f48dc51b2
Jorge <jorgeevangelista500@gmail.com>
2022-01-24 18:08:59 -0500

    added show test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,33 +4,31 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {
-    render (<Episode episode={{
+const testEpisode={
         id: 0,
-        image: 'string',
-        name: 'string',
+        image: null,
+        name: 'Jorge',
         season: 0,
         number: 0,
-        summary: 'string',
-        runtime: 'string',
-      }} />);
+        summary: 'summary 1',
+        runtime: 45,
+}
+
+test("renders without error", () => {
+    render (<Episode episode = {testEpisode}/>);
 });
 
 test("renders the summary test passed as prop", ()=>{
-    render (<Episode episode={
-        {id: 1,name: 'jorge', season:1, number:1, summary:'summary 1', runtime:45 }} />);
+    render (<Episode episode={testEpisode} />);
 
     const episodeSummary = screen.getByText(/summary 1/i)
     expect(episodeSummary).toBeInTheDocument();
 });
 
 test("renders default image when image is not defined", ()=>{
-    render (<Episode episode={
-        {id: 1, name: 'jorge2', season:1, number:1, summary:'summary 2', runtime:45, image:null }} />);
+    render (<Episode episode={testEpisode} />);
 
         const imgAlt = screen.getByAltText("https://i.ibb.co/2FsfXqM/stranger-things.png");
-        console.log(imgAlt)
         expect(imgAlt).toBeInTheDocument();
-        
-
+ 
 });
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
+import { getByLabelText, queryAllByTestId, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Episode from '../Episode';
+import { query } from 'express';
 
-test('renders without errors', ()=>{});
+const testShow = {
+    name: 'string',
+    summary: 'string',
+    seasons: [
+        {
+            id:0,
+            name: 'string',
+            episodes: []
+        },
+        {
+            id:1,
+            name: 'string2',
+            episodes: []
+        },
+    ]
+}
+       
 
-test('renders Loading component when prop show is null', () => {});
+test('renders without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
+});
+
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} />)
+    const isLoading = screen.getByTestId('loading-container')
+    expect(isLoading).toBeInTheDocument();
+});
 
 
-test('renders same number of options seasons are passed in', ()=>{});
+test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
 
-test('handleSelect is called when an season is selected', () => {});
+    const numberSeasons = screen.queryAllByTestId("season-option")
+    expect(numberSeasons).toHaveLength(2);
+})
+    
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {});
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn()
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>)
+
+    const seasonSelector = screen.getByLabelText(/Select A Season/i);
+    userEvent.selectOptions(seasonSelector, ['0']);
+
+    expect(handleSelect).toBeCalled();
+
+
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={testShow} selectedSeason={'none'}/>);
+    let episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={testShow} selectedSeason={0}/>)
+    episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).toBeInTheDocument();
+});
+    
